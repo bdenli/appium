29208a0df03235c00d976d19278fbf5d34befd2c
Basak Denli <basakgdenli@gmail.com>
2023-10-23 13:35:37 -0400

    Add files via upload

---
+++ copy_zshrc.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home
+#PATH="/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home/bin:$PATH"
+#export PATH=$PATH:$JAVA_HOME/bin
